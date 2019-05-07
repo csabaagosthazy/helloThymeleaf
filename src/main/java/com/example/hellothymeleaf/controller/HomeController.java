7839508cc58ecac75cc45bb60ab0212ee97b189b
csabaagosthazy <agosthazy.csaba@gmail.com>
2019-05-07 21:11:17 +0300

    initial commit

---
+++ src/main/java/com/example/hellothymeleaf/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.hellothymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value="name")String name, @RequestParam(value="age")int age, Model model) {
+		
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		return "hello";
+	}
+}
